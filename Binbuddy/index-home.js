ca0f4f2d90a77979dbcc4b75d4eaac4be8d02381
Abhi01P <143483127+Abhi01P@users.noreply.github.com>
2024-07-03 23:46:42 +0530

    Add files via upload

---
+++ Binbuddy/index-home.js
@@ -0,0 +1,48 @@
+var aBOUTText = document.getElementById("aBOUTText");
+      if (aBOUTText) {
+        aBOUTText.addEventListener("click", function (e) {
+          window.location.href = "./about.html";
+        });
+      }
+      
+      var lOGINText = document.getElementById("lOGINText");
+      if (lOGINText) {
+        lOGINText.addEventListener("click", function (e) {
+          window.location.href = "./l-o-g-i-n.html";
+        });
+      }
+      
+      var signUp = document.getElementById("signUp");
+      if (signUp) {
+        signUp.addEventListener("click", function (e) {
+          window.location.href = "./role-index.html";
+        });
+      }
+      
+      var gETYOURBREAKFAST = document.getElementById("gETYOURBREAKFAST");
+      if (gETYOURBREAKFAST) {
+        gETYOURBREAKFAST.addEventListener("click", function (e) {
+          window.location.href = "./meal.html";
+        });
+      }
+      
+      var gETYOURLUNCH = document.getElementById("gETYOURLUNCH");
+      if (gETYOURLUNCH) {
+        gETYOURLUNCH.addEventListener("click", function (e) {
+          window.location.href = "./meal.html";
+        });
+      }
+      
+      var gETYOURSNACKS = document.getElementById("gETYOURSNACKS");
+      if (gETYOURSNACKS) {
+        gETYOURSNACKS.addEventListener("click", function (e) {
+          window.location.href = "./meal.html";
+        });
+      }
+      
+      var gETYOURDINNER = document.getElementById("gETYOURDINNER");
+      if (gETYOURDINNER) {
+        gETYOURDINNER.addEventListener("click", function (e) {
+          window.location.href = "./meal.html";
+        });
+      }
